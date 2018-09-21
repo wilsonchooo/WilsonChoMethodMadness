39332826243e8558d7f3bd74fd7a14f7ac147dc0
wilsonchooo <43213133+wilsonchooo@users.noreply.github.com>
2018-09-21 11:42:47 -0400

    Add files via upload

--- src/Wilson/Main.java
+++ src/Wilson/Main.java
@@ -3,11 +3,11 @@ package Wilson;
 public class Main {
 
     public static void main(String[] args) {
-        //   System.out.println(WChoLib.isPalindrome("racecar"));
-        // System.out.println(WChoLib.dateStr("01/08/1999"));
-       // System.out.println(WChoLib.sumUpTo(12));
-        //System.out.println(WChoLib.primePrinter(12));
-        System.out.println(WChoLib.quadSolver(5.0,6.0,1.0));
+        //   System.out.println(Wilson.WChoLib.isPalindrome("racecar"));
+        // System.out.println(Wilson.WChoLib.dateStr("01/08/1999"));
+       // System.out.println(Wilson.WChoLib.sumUpTo(12));
+        //System.out.println(Wilson.WChoLib.quadSolver(5.0,6.0,1.0));
+        Wilson.WChoLib.multiplicationTable(3,3);
 
 
 
--- src/Wilson/WChoLib.java
+++ src/Wilson/WChoLib.java
@@ -3,11 +3,10 @@ package Wilson;
 public class WChoLib {
 
 
-    public static boolean isPalindrome(String str){
-        String newWord="";
-        for(int i = str.length()-1; i>=0; i--)
-        {
-            newWord += str.substring(i,i+1);
+    public static boolean isPalindrome(String str) {
+        String newWord = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            newWord += str.substring(i, i + 1);
         }
 
         if (str.equals(newWord))
@@ -15,67 +14,45 @@ public class WChoLib {
 
         else return false;
     }
-    public static String dateStr(String str)
 
-    {
+    public static String dateStr(String str) {
         String date = "";
-        date += str.substring(3,5)+"-" + str.substring(0,2)+"-" +str.substring(6);
+        date += str.substring(3, 5) + "-" + str.substring(0, 2) + "-" + str.substring(6);
         return date;
     }
 
-    public static int sumUpTo(int num)
-    {
+    public static int sumUpTo(int num) {
         int storer = 0;
-        for (int x=0; x<=num; x++)
-        {
+        for (int x = 0; x <= num; x++) {
             storer += x;
         }
         return storer;
     }
 
-/*
-   public static String primePrinter(int num)
-    {
-        String holder = "";
-        for (int x = 0; x < num; x++) {
-            if (x % 2 != 0 && x % 3 != 0) {
-                holder += ", " + x;
-            }
-        }
-        return holder.substring(2);
-    }
-*/
-
-    public static String primePrinter(int num)
-    {
-        String holder = "";
-        for (int x = 0; x < num; x++) {
-            if (Integer.MAX_VALUE % 2 != 0 && Integer.MAX_VALUE % 3 != 0) {
-                holder += ", " + x;
-            }
-        }
-        return holder.substring(2);
-    }
-
-    public static String quadSolver(Double a, Double b, Double c)
-    {
-        double x = (b*b) - (4 * a * c);
-        if (x>0)
-        {
-            Double root = (-b + Math.sqrt(b*b - 4*a*c ))/(2*a);
-            Double root2 = (b + Math.sqrt(b*b - 4*a*c ))/(2*a);
 
 
-            return Double.toString(root) +", " + Double.toString(root2);
-        }
-        else if (x<0)
-        {
+
+    public static String quadSolver(Double a, Double b, Double c) {
+        double x = (b * b) - (4 * a * c);
+        if (x > 0) {
+            Double root = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+            Double root2 = (b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+
+
+            return Double.toString(root) + ", " + Double.toString(root2);
+        } else if (x < 0) {
             return "roots are imaginary";
         }
 
-        return "LMAO DUMB BITCH";
+        return " Pleae Enter Again ";
+    }
+
+
+    public static void multiplicationTable(int base, int range) {
+        for (int x = 0; x <= range; x++) {
+            System.out.println(base * x);
+        }
     }
 
 
 }
-
