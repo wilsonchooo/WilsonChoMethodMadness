99196de737dad5ed9e8dc700a11bb6b9eb677daa
wilsonchooo <43213133+wilsonchooo@users.noreply.github.com>
2018-09-23 23:01:22 -0400

    Add files via upload

--- src/Wilson/Main.java
+++ src/Wilson/Main.java
@@ -3,11 +3,11 @@ package Wilson;
 public class Main {
 
     public static void main(String[] args) {
-        //   System.out.println(Wilson.WChoLib.isPalindrome("racecar"));
-        // System.out.println(Wilson.WChoLib.dateStr("01/08/1999"));
-       // System.out.println(Wilson.WChoLib.sumUpTo(12));
-        //System.out.println(Wilson.WChoLib.quadSolver(5.0,6.0,1.0));
-        Wilson.WChoLib.multiplicationTable(3,3);
+        System.out.println(Wilson.WChoLib.isPalindrome("racecar")); // Runs the isPalindrome program located in the class WChoLib with the specified parameters as the string that will be test and prints it.
+        System.out.println(Wilson.WChoLib.dateStr("01/08/1999")); // Runs the dateStr program located in the class WChoLib with the specified date and  rearranges it. It is then printed.
+        System.out.println(Wilson.WChoLib.sumUpTo(3)); // Runs the sumUpTo program located in the class WChoLib with the and adds up the numbers to the number in the parameter and prints it.
+        System.out.println(Wilson.WChoLib.quadSolver(5.0,6.0,1.0)); // Runs the quadSolver program located in the class WChoLib with the A, B, and C variables parameters and prints it.
+        Wilson.WChoLib.multiplicationTable(3,3); // Runs the multiplicationTable program located in the class WChoLib with the specified base and range and prints it.
 
 
 
--- src/Wilson/WChoLib.java
+++ src/Wilson/WChoLib.java
@@ -6,25 +6,27 @@ public class WChoLib {
     public static boolean isPalindrome(String str) {
         String newWord = "";
         for (int i = str.length() - 1; i >= 0; i--) {
-            newWord += str.substring(i, i + 1);
+            newWord += str.substring(i, i + 1); // For loop that recreates the word backward.
         }
 
-        if (str.equals(newWord))
+        if (str.equals(newWord)) // checks the newly created word if it is the same. If it is, it will return true;
             return true;
 
         else return false;
     }
 
+
     public static String dateStr(String str) {
         String date = "";
-        date += str.substring(3, 5) + "-" + str.substring(0, 2) + "-" + str.substring(6);
+        date += str.substring(3, 5) + "-" + str.substring(0, 2) + "-" + str.substring(6); // builds a new string by adding different parts of the original through the use of substring.
         return date;
     }
+
 
     public static int sumUpTo(int num) {
         int storer = 0;
         for (int x = 0; x <= num; x++) {
-            storer += x;
+            storer += x; // A for loop that continually adds up the previous numbers until it has reached the number in the parameter.
         }
         return storer;
     }
@@ -32,27 +34,26 @@ public class WChoLib {
 
 
 
+    public static void multiplicationTable(int base, int range) {
+        for (int x = 0; x <= range; x++) {
+            System.out.println(base * x); //Prints out the base given in the parameters times the range.
+        }
+    }
+
+
+
     public static String quadSolver(Double a, Double b, Double c) {
         double x = (b * b) - (4 * a * c);
-        if (x > 0) {
-            Double root = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
-            Double root2 = (b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+        if (x > 0) { // determines if the root will be negative or positive.
+            Double root = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a); // The quadratic formula is done using A, B, and C specified in the parameters.
+            Double root2 = (b + Math.sqrt(b * b - 4 * a * c)) / (2 * a); // A root has both a negative and positive as shown in the +- in the formula.
 
 
-            return Double.toString(root) + ", " + Double.toString(root2);
+            return Double.toString(root) + ", " + Double.toString(root2); // creates a string that displays the roots.
         } else if (x < 0) {
             return "roots are imaginary";
         }
 
-        return " Pleae Enter Again ";
+        return " Please Enter Again ";
     }
-
-
-    public static void multiplicationTable(int base, int range) {
-        for (int x = 0; x <= range; x++) {
-            System.out.println(base * x);
-        }
-    }
-
-
 }
